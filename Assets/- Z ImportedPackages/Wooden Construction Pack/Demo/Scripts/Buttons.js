45bcefb6b940773f1a84b0f0910a552ea384af48
Dgooding <dgoodingindi@gmail.com>
2015-08-18 02:08:11 -0400

    Mine1

---
+++ Assets/- Z ImportedPackages/Wooden Construction Pack/Demo/Scripts/Buttons.js
@@ -0,0 +1,57 @@
+﻿private var LevelsName : String[] = [ 
+    "Cavern", 
+    "Area",
+    "Rocky Road" ] ;
+private var label_play = ">";
+var anim : Animator; 
+private var buttonPressed : boolean = true;
+function Start () {
+	
+	if (anim != null)
+		anim.speed = 0;
+	
+}
+
+function OnGUI() {
+	
+	for (var i = 0; i <= 2 ; i++)
+	{
+		GUI.color = Color.cyan; 
+		if (i == Application.loadedLevel)
+		{
+ 			GUI.enabled=false;
+ 			GUI.color = Color.white;
+			GUI.Button(Rect((Screen.width / 2-600)+i*200,10,150,50), LevelsName[i]);
+			GUI.enabled=true;
+		} 
+		else
+		{
+			if (GUI.Button(Rect((Screen.width / 2-600)+i*200,10,150,50), LevelsName[i]))
+				Application.LoadLevel(i);
+			if (anim != null)
+			{
+				if (GUI.Button(Rect((Screen.width / 2),(Screen.height / 2+300),50,50), label_play ))
+				{
+					if (buttonPressed)
+						{ 
+							label_play = "||";		
+							anim.speed = 1;
+							buttonPressed = false;
+						}
+						else
+						{
+							label_play = ">";		
+							anim.speed = 0;
+							buttonPressed = true;
+						}
+				}
+				if (GUI.Button(Rect((Screen.width / 2-50),(Screen.height / 2+300),50,50), "<<"))
+						anim.speed = -3;
+				if (GUI.Button(Rect((Screen.width / 2+50),(Screen.height / 2+300),50,50), ">>"))
+						anim.speed = 3;
+			}
+		}
+		
+	}
+					
+}
