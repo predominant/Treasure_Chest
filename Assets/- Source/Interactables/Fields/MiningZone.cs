f0f655c9ca76cfa04b25078b42bb68b035331eed
Tom Brooks <tom@foxcubgames.com>
2015-08-22 23:21:17 -0400

    Initial port of old mining and field code from SB

---
+++ Assets/- Source/Interactables/Fields/MiningZone.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Gamelogic.Grids;
+
+public class MiningZone : TileZone
+{
+    //public List<GameObject> m_MiningNodePrefabs = null;
+
+    //public GameObject[,] m_Nodes = null;
+    //public Texture2D m_CurrentGenMask = null;
+    //public List<Texture2D> m_GenerationMasks = null;
+    ////public Texture2D m_NoiseMap = null;
+
+    //protected FarmPlot m_FarmPlot = null;
+	
+    //public bool m_UseNoiseGeneration = true;
+    //public bool m_UseRandomGeneration = true;
+    //public bool m_DbgGenerateTimePaused = true;
+    //public float m_DbgGenerateDuration = 0.5f;
+    //protected float m_DbgGenerateTimer = 0f;
+	
+    //// Use this for initializationss
+    //protected override void Start()
+    //{
+    //    base.Start();
+
+    //    ResizeNodesArray();
+    //}
+
+    //protected override void Update ()
+    //{
+    //    base.Update();
+
+    //    if( !m_DbgGenerateTimePaused )
+    //        return;
+
+    //    m_DbgGenerateTimer += Time.deltaTime;
+    //    if( m_DbgGenerateTimer < m_DbgGenerateDuration )
+    //        return;
+
+    //    m_DbgGenerateTimer = 0f;
+    //    ResizeNodesArray();
+    //}
+
+    //void ResizeNodesArray()
+    //{
+    //    if( null != m_Nodes )
+    //        foreach( GameObject node in m_Nodes )
+    //            if( null != node )
+    //                DestroyImmediate( node );
+
+    //    m_Nodes = new GameObject[ (int)m_GridSize.x, (int)m_GridSize.y ];
+		
+    //    for( int x = 0; x < m_GridSize.x; ++x )
+    //    {
+    //        for( int y = 0; y < m_GridSize.y; ++y )
+    //        {
+    //            /*int ix = x % 2;
+    //            int iy = y % 2;
+
+    //            if( ix == 0 && iy == 0 || ix == 1 && iy == 1 )
+    //            {
+    //                GameObject node = (GameObject)GameObject.Instantiate( m_MiningNodePrefab );
+    //                node.transform.localPosition = Vector3.zero;
+    //                node.transform.position = new Vector3( m_MinCellPoint.x + x, m_MinCellPoint.y, m_MinCellPoint.z + y );
+    //                m_Nodes[x,y] = node;
+    //            }
+    //            else
+    //                m_Nodes[x,y] = null;*/
+
+
+    //            Color pixel = Color.white;
+
+    //            if( m_GenerationMasks.Count > 0 )
+    //            {
+    //                m_CurrentGenMask = m_GenerationMasks[ Random.Range(0, m_GenerationMasks.Count) ];
+
+    //                if( null != m_CurrentGenMask )
+    //                    pixel = m_CurrentGenMask.GetPixel( x, y );
+    //            }
+
+    //            float noise = 1f;
+
+    //            if( m_UseNoiseGeneration )
+    //                noise = Mathf.PerlinNoise( m_MinCell.X + x, m_MinCell.Y + y );
+
+    //            float nodeChanceValue = pixel.b * noise;
+    //            float nodeRandChance = 0f;
+
+    //            if( m_UseRandomGeneration )
+    //                nodeRandChance = Random.value;
+
+    //            if( nodeChanceValue > nodeRandChance )
+    //                CreateMiningNode( x, y, pixel.g, pixel.r );
+    //            else
+    //                m_Nodes[x,y] = null;
+    //        }
+    //    }
+    //}
+
+    //public void CreateMiningNode( int x, int y, float rarityP = 1f, float yieldP = 1f )
+    //{
+    //    if( null != m_Nodes[x,y] )
+    //        DestroyImmediate( m_Nodes[x,y] );
+
+    //    int prefabIdx = 0;
+
+    //    if( m_UseRandomGeneration )
+    //        prefabIdx = Random.Range(0, Mathf.RoundToInt(m_MiningNodePrefabs.Count * rarityP));
+    //    else
+    //        prefabIdx = Mathf.Clamp( Mathf.RoundToInt(m_MiningNodePrefabs.Count * rarityP), 0, m_MiningNodePrefabs.Count-1 );
+
+    //    GameObject node = (GameObject)GameObject.Instantiate( m_MiningNodePrefabs[prefabIdx] );
+    //    node.transform.localPosition = Vector3.zero;
+    //    node.transform.position = new Vector3( m_MinCellPoint.x + x, m_MinCellPoint.y, m_MinCellPoint.z + y );
+
+    //    MiningNode nodeScript = node.GetComponent<MiningNode>();
+    //    nodeScript.m_Yield = nodeScript.m_MaxYield;//Mathf.RoundToInt(nodeScript.m_MaxYield * yieldP * Random.value);
+    //    m_Nodes[x,y] = node;
+    //}
+}
---
+++ Assets/- Source/Interactables/Fields/TileZone.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using Gamelogic.Grids;
+
+//[RequireComponent( typeof( ColliderBoundsEx ) )]
+public class TileZone : MonoBehaviour
+{
+    //public Vector3 m_MinPoint = Vector3.zero;
+    //public Vector3 m_MaxPoint = Vector3.zero;
+    //public Vector3 m_MinCellPoint = Vector3.zero;
+    //public Vector3 m_MaxCellPoint = Vector3.zero;
+    //public RectPoint m_MinCell = RectPoint.Zero;
+    //public RectPoint m_MaxCell = RectPoint.Zero;
+
+    //public Vector3 m_Centroid = Vector3.zero;
+
+    //public Vector2 m_Size = Vector2.zero;
+    //public Vector2 m_GridSize = Vector2.zero;
+
+    //protected bool m_SelectionOnGrid = false;
+    //protected RectPoint m_SelectedPoint = RectPoint.Zero;
+    //protected Vector3 m_SelectedPosition = Vector3.zero;
+    ////protected ToolFramework m_ToolFramework = null;
+    //protected GameObject m_ToolField = null;
+
+    //protected virtual void Start()
+    //{
+    //    //m_ToolFramework = GameObject.Find( "Player" ).GetComponent<ToolFramework>();
+		
+    //    ColliderBoundsEx bounds = GetComponent<ColliderBoundsEx>();
+		
+    //    m_MinPoint = bounds.GetColliderVertex( ColliderBoundsEx.VertexType.MinBL );
+    //    m_MaxPoint = bounds.GetColliderVertex( ColliderBoundsEx.VertexType.MaxBR );
+		
+    //    m_MinCell = WorldGridMap.Instance.GetClosestCell( m_MinPoint );
+    //    m_MaxCell = WorldGridMap.Instance.GetClosestCell( m_MaxPoint );
+		
+    //    m_MinCellPoint = WorldGridMap.Instance.GetCellPosition( m_MinCell );
+    //    m_MaxCellPoint = WorldGridMap.Instance.GetCellPosition( m_MaxCell );
+    //    m_MinCellPoint.y = m_MaxCellPoint.y = transform.localScale.y * 0.5f * 0f;
+		
+    //    m_Centroid = (m_MaxCellPoint-m_MinCellPoint)*0.5f + m_MinCellPoint;
+    //    m_Size.x = m_MaxCellPoint.x-m_MinCellPoint.x + WorldGridMap.Instance.CellWidth;
+    //    m_Size.y = m_MaxCellPoint.z-m_MinCellPoint.z + WorldGridMap.Instance.CellHeight;
+		
+    //    transform.position = m_Centroid;
+    //    transform.localScale = new Vector3( m_Size.x, transform.localScale.y, m_Size.y );	
+		
+    //    m_GridSize.x = Mathf.Abs( m_MaxCell.X - m_MinCell.X ) + 1;
+    //    m_GridSize.y = Mathf.Abs( m_MaxCell.Y - m_MinCell.Y ) + 1;
+    //}
+
+    //// Update is called once per frame
+    //protected virtual void Update()
+    //{
+    //    if( null == m_ToolField )
+    //        return;
+
+    //    Debug.DrawLine( m_MinCellPoint, m_MinCellPoint + Vector3.up * 10f, Color.green );
+    //    Debug.DrawLine( m_MaxCellPoint, m_MaxCellPoint + Vector3.up * 10f, Color.red );
+		
+    //    //m_SelectedPoint = WorldGridMap.Instance.GetClosestCell( m_ToolFramework.m_ToolHighlight.transform.position );
+    //    m_SelectionOnGrid = PointInZone( m_SelectedPoint );
+    //    //m_SelectedPosition = WorldGridMap.Instance.GetCellPosition( m_SelectedPoint );
+		
+    //    // If the selection point is not on the grid, return
+    //    //if( m_SelectionOnGrid )
+    //        //m_ToolFramework.m_ToolHighlightActive = true;
+    //    //else
+    //    //{
+    //        //m_ToolFramework.m_ToolHighlightActive = false;
+    //        //return;
+    //    //}
+    //}
+	
+    //protected bool PointInZone( RectPoint point )
+    //{
+    //    if( point.X >= m_MinCell.X &&
+    //       point.X <= m_MaxCell.X &&
+    //       point.Y >= m_MinCell.Y &&
+    //       point.Y <= m_MaxCell.Y )
+    //        return true;
+		
+    //    return false;
+    //}
+	
+    //public void SetToolField( GameObject toolField )
+    //{
+    //    m_ToolField = toolField;
+    //}
+	
+    //public void ClearToolField()
+    //{	
+    //    m_ToolField = null;
+    //    //m_ToolFramework.m_ToolHighlightActive = false;
+    //}
+}
